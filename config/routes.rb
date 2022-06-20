934824c1574d1b2232d29bd43c9d2b3118fe90e8
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-20 15:43:48 -0500

    Las devise changes and rubocop offenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
+  devise_scope :user do
+    get '/sign_out', to: 'users/sessions#destroy'
+  end
 end
