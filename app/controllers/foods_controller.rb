859237f8a7e4d0769cf68c970b52fcf611ebf1cd
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-21 16:45:33 -0500

    Controller refactor

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -2,4 +2,31 @@ class FoodsController < ApplicationController
   def index
     @foods = current_user.foods
   end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+
+    redirect_to foods_path, notice: 'Food successfully deleted'
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = current_user.foods.new(food_params)
+
+    if @food.save
+      redirect_to foods_path, notice: 'Food type was successfully added'
+    else
+      render :new, alert: 'Food could not be added'
+    end
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end
 end
