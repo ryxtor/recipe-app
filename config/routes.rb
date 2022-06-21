4a3a90f397fc451731c8cff78fb8cfef8aff0c65
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-22 02:20:04 +0300

    change date field t string

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'users#index'
+  root 'users#index'
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
   devise_scope :user do
