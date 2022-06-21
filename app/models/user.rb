dc3bd84dabf90ca9b701b09138d3b2982ef605ed
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-22 02:29:40 +0300

    fix rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-
   has_many :recipes, foreign_key: :user_id, class_name: 'Recipe', dependent: :delete_all
   has_many :inventories, foreign_key: :user_id, class_name: 'Inventory', dependent: :delete_all
 end
