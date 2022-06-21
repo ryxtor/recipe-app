b96abcace294269640a18571d1dc6ee669a95996
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-21 19:49:13 +0300

    added get route to t routes for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
+  get '/users/:id/', to: 'recipes#index'
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
   devise_scope :user do
