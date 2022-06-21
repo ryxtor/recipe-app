bd6970e26758dd5c3fedeb4cc215286601b11f04
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-22 01:40:50 +0300

    modify recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,34 @@
 class RecipesController < ApplicationController
+    load_and_authorize_resource
     def index; 
-        @recipes = Recipe.all
+        @recipes = current_user.recipes.all
     end
 
+    def create
+        @recipe = current_user.recipes.new(recipe_params)
+        if @recipe.save
+          flash[:success] = 'Your recipe has been created!'
+          redirect_to recipes_path
+        else
+          flash[:error] = 'Inventory not created!'
+          redirect_to new_recipe_path
+        end
+    end
+   
+
     def show
-        
+       @recipe = current_user.recipes.find(params[:id])
+    end
+
+    def destroy
+        @recipe = current_user.recipes.find(params[:id])
+        @recipe.destroy
+        redirect_to '/recipes'
+    end
+
+    private
+    def recipe_params
+      params.require(:recipe).permit(:name, :preparation_time, :coking_time, :description, :public)
     end
 end
   
