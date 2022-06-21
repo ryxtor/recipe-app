f438b9811a9e9cc113c160b12856a21d020a7acf
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-21 18:45:09 -0500

    Fixing name of controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,4 +1,4 @@
-class RecipesController < ApplicationController
+class PublicRecipesController < ApplicationController
   def index
     @pub_recipes = Recipe.includes(:user, [:foods], :recipe_foods).where(public: true)
   end
