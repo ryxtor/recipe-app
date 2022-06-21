099d71e9e1963524d338c3baed84dff9c17d9834
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-21 20:56:40 +0300

    configure recipe item route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
   get '/users/:id/', to: 'recipes#index'
+  get '/users/:id/recipes/:id', to: 'recipes#show'
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
   devise_scope :user do
