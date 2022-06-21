83788138cb3c4e89583be103f5780c688b0d4f67
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-21 16:03:00 -0500

    Adding foods to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :foods, foreign_key: :user_id, class_name: 'Food', dependent: :delete_all
 end
