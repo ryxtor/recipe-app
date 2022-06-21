215e28bfd2ce63073887a21baf7e33fbd30553b6
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-21 13:30:15 -0300

    Add relationships

---
+++ app/models/inventory.rb
@@ -0,0 +1,4 @@
+class Inventory < ActiveRecord::Base
+  belongs_to :user, class_name: 'User'
+  has_many :inventory_foods, foreign_key: :inventory_id, class_name: 'InventoryFood'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :inventories, foreign_key: :user_id, class_name: 'Inventory', dependent: :delete_all
 end
