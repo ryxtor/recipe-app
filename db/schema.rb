344acf4cdfae2eda17cc5eb34e1a80a5f85eb5e5
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-22 02:18:41 +0300

    changes timestamps to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_025855) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_230610) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,8 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_21_025855) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.datetime "preparation_time"
-    t.datetime "cooking_time"
+    t.string "preparation_time"
+    t.string "cooking_time"
     t.text "description"
     t.boolean "public"
     t.datetime "created_at", null: false
