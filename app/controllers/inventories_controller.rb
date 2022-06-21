d3d892144074cc036c07ce8bb3c8a38efdfa4821
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-21 13:29:29 -0300

    Create inventories controller

---
+++ app/controllers/inventories_controller.rb
@@ -0,0 +1,26 @@
+class InventoriesController < ApplicationController
+  def index
+    @inventories = current_user.inventories.all
+  end
+
+  def create
+    @inventory = current_user.inventories.new(inventory_params)
+    if @inventory.save
+      redirect_to inventories_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @inventory = current_user.inventories.find(params[:id])
+    @inventory.destroy
+    redirect_to inventories_path
+  end
+
+  private
+
+  def inventory_params
+    params.require(:inventory).permit(:name)
+  end
+end
