889aab1b3bac4c8f457a708011e8b2cdde9d7dd0
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-06-22 19:48:53 -0300

    Add heroku domain

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     return unless user.present?
 
-    can :read, :all
+    can :read, Inventory, user_id: user.id
     can :manage, Recipe, user_id: user.id
     can :manage, Inventory, user_id: user.id
 
