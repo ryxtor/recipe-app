d8d5a78b3631a2340fc455f359a84ce7d1082175
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-22 22:26:13 -0500

    Adding total price formula

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -2,5 +2,10 @@ class ShoppingListController < ApplicationController
   def index
     @inventory = Inventory.first
     @foods = InventoryFood.where(inventory_id: @inventory.id).includes([:food])
+
+    @prices = 0
+    @foods.each do |food|
+      @prices += (food.quantity * food.food.price)
+    end
   end
 end
