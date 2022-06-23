f49d2564c585b1601abe4e8e62e210a1e544fb29
Kakalanp <jgarridoleccazanetti@gmail.com>
2022-06-23 16:36:38 -0500

    Adding method to compare ingredients in controller

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,11 +1,34 @@
 class ShoppingListController < ApplicationController
   def index
-    @inventory = Inventory.first
-    @foods = InventoryFood.where(inventory_id: @inventory.id).includes([:food])
-
+    @inventory = Inventory.find(params[:inventory_id])
+    @recipe = Recipe.find(params[:recipe_id])
+    @foods = []
     @prices = 0
-    @foods.each do |food|
-      @prices += (food.quantity * food.food.price)
+
+    food_helper
+  end
+
+  private
+
+  def food_helper
+    i_foods = InventoryFood.where(inventory_id: @inventory.id).includes([:food])
+    r_foods = RecipeFood.where(recipe_id: @recipe.id).includes([:food])
+
+    r_foods.each do |r_food|
+      i_foods.each do |i_food|
+        if r_food.food.id == i_food.food.id
+          if r_food.quantity > i_food.quantity
+            quantity = r_food.quantity - i_food.quantity
+            newfood = r_food
+            newfood.quantity = quantity
+            @foods.push(newfood)
+            @prices += (quantity * i_food.food.price)
+          end
+        else
+          @foods.push(r_food)
+          @prices += (r_food * i_food.food.price)
+        end
+      end
     end
   end
 end
